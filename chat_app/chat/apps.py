393fdf080112c5cc2a1aa974780db727684d63a7
Ilya <ilya.makarochkin@yandex.ru>
2024-05-09 20:29:26 +0500

    rename

--- chat_app/chat/apps.py
+++ chat_app/chat/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ModConfig(AppConfig):
+class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mod'
+    name = 'chat'
